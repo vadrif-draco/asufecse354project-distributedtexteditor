9cd4dffe8115342f0cbc236d7e19792d41077cee
muhammad-ashraf-ab0 <muhammad.ashrafab0@gmail.com>
2022-06-26 15:04:05 +0000

    nginx routes all pages more accurately start-server.sh edits Angular environment variables

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,7 @@
 # Set environment variable with current hostname
 export API_HOST=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 
+# Change nginx configuration file to match new hostname
 /bin/bash ./deployment_scripts/frontend/nginx-change-host.sh
 
+sed -i "s/^[ \t]*hostname: '[a-zA-Z0-9\.-]*'$/  hostname: '$API_HOST'/" ./src/environments/environment.prod.ts
